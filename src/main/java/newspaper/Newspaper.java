c9c55805422b9db4e39eeae4f0711c395484678e
Karim Omar <komar@student.tgm.ac.at>
2019-06-03 13:42:32 +0200

    Newspaper-Objekt definiert

--- src/main/java/newspaper/Newspaper.java
+++ src/main/java/newspaper/Newspaper.java
@@ -1,4 +1,13 @@
 package newspaper;
 
 public class Newspaper {
+    private String name;
+    private String headline;
+    private String context;
+
+    public Newspaper(){
+        this.name="";
+        this.headline="";
+        this.context="";
+    }
 }
